729aa73fa013d5af03d3e97282799fd8f0acd397
YISHON <apple@YISHON.local>
2021-03-25 09:58:33 +0800

    add profile

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"os"
+	"runtime/pprof"
 	"yishon.github.com/optimization-in-golang/dealFunc"
 )
 
 func main() {
 
 	//pprof open code
-	//cpuProfile, _ := os.Create("cpu_profile")
-	//pprof.StartCPUProfile(cpuProfile)
-	//defer pprof.StopCPUProfile()
+	cpuProfile, _ := os.Create("cpu_profile")
+	pprof.StartCPUProfile(cpuProfile)
+	defer pprof.StopCPUProfile()
 
 
 	//待优化的原生func
@@ -19,7 +21,7 @@ func main() {
 	//run result:
 	//"moby.txt": 428544 words
 	//go run main.go moby.txt  0.27s user 0.18s system 70% cpu 0.641 total
-	dealFunc.Func2()
+	//dealFunc.Func2()
 
 	//优化第二版的func
 	//dealFunc.Func3()
